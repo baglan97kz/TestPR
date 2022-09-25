44e711f7bebd8a6f167381e5d8f98cd6e778c204
Tolebay Baglan <baglantulebay@gmail.com>
2022-09-25 20:41:23 +0600

    czxcxc

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,25 +2,4 @@
 //sdgf
 Console.WriteLine("Hello, World!");
 
-Person a = new Person(5000);
-a.Account = 200;
-a.Print();
-
-class Person {
-    int account;
-// changed 123456
-// second change
-    public Person(int account){
-        this.account = account;
-
-    }
-    public int Account
-    {
-
-        get { return account; } 
-        set { account = value; }
-    }
-
-    internal void Print() =>  Console.WriteLine(account);
-
-}
+//ghjkhjk
